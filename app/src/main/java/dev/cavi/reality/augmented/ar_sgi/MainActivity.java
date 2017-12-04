005e6483c3a1260ba78189a765b469f9e2fa09de
Marc Vila <marcvilagomez@gmail.com>
2017-12-04 19:42:43 +0100

    Base app with new ARToolkit Lib

---
+++ app/src/main/java/dev/cavi/reality/augmented/ar_sgi/MainActivity.java
@@ -0,0 +1,13 @@
+package dev.cavi.reality.augmented.ar_sgi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
