c1553acdea00c1839a94aa8ebdf0457b9a0e7df2
Marc <marcvila10@hotmail.com>
2017-12-10 23:02:39 +0100

    Adding 6 new patterns (ABCDFG)

--- app/src/main/java/dev/cavi/reality/augmented/ar_sgi/MyRenderer.java
+++ app/src/main/java/dev/cavi/reality/augmented/ar_sgi/MyRenderer.java
@@ -18,10 +18,22 @@ public class MyRenderer extends ARRenderer {
     private String TAG = MyRenderer.class.getSimpleName();
     private int markerHiro = -1;
     private int markerKanji = -1;
+    private int markerA = -1;
+    private int markerB = -1;
+    private int markerC = -1;
+    private int markerD = -1;
+    private int markerG = -1;
+    private int markerF = -1;
     private List<Integer> markerArray = new ArrayList<>();
 
-    private Cube cube1 = new Cube(50.0f, 0.0f, 0.0f, 20.0f);
-    private Cube cube2 = new Cube(60.0f, 0.0f, 0.0f, 10.0f);
+    private Cube cube1 = new Cube(10.0f, 0.0f, 0.0f, 20.0f);
+    private Cube cube2 = new Cube(20.0f, 0.0f, 0.0f, 20.0f);
+    private Cube cube3 = new Cube(30.0f, 0.0f, 0.0f, 20.0f);
+    private Cube cube4 = new Cube(40.0f, 0.0f, 0.0f, 20.0f);
+    private Cube cube5 = new Cube(50.0f, 0.0f, 0.0f, 20.0f);
+    private Cube cube6 = new Cube(60.0f, 0.0f, 0.0f, 20.0f);
+    private Cube cube7 = new Cube(70.0f, 0.0f, 0.0f, 20.0f);
+    private Cube cube8 = new Cube(80.0f, 0.0f, 0.0f, 20.0f);
     private float angle = 0.0f;
     private boolean spinning = false;
 
@@ -39,8 +51,50 @@ public class MyRenderer extends ARRenderer {
             return false;
         }
 
+        markerA = ARToolKit.getInstance().addMarker("single;Data/multi/patt.a;80");
+        if (markerA < 0) {
+            Log.e(TAG, "Unable to load markerA");
+            return false;
+        }
+
+        markerB = ARToolKit.getInstance().addMarker("single;Data/multi/patt.b;80");
+        if (markerB < 0) {
+            Log.e(TAG, "Unable to load markerB");
+            return false;
+        }
+
+        markerC = ARToolKit.getInstance().addMarker("single;Data/multi/patt.c;80");
+        if (markerC < 0) {
+            Log.e(TAG, "Unable to load markerC");
+            return false;
+        }
+
+        markerD = ARToolKit.getInstance().addMarker("single;Data/multi/patt.d;80");
+        if (markerD < 0) {
+            Log.e(TAG, "Unable to load markerD");
+            return false;
+        }
+
+        markerF = ARToolKit.getInstance().addMarker("single;Data/multi/patt.f;80");
+        if (markerF < 0) {
+            Log.e(TAG, "Unable to load markerF");
+            return false;
+        }
+
+        markerG = ARToolKit.getInstance().addMarker("single;Data/multi/patt.g;80");
+        if (markerG < 0) {
+            Log.e(TAG, "Unable to load markerG");
+            return false;
+        }
+
         markerArray.add(markerHiro);
         markerArray.add(markerKanji);
+        markerArray.add(markerA);
+        markerArray.add(markerB);
+        markerArray.add(markerC);
+        markerArray.add(markerD);
+        markerArray.add(markerF);
+        markerArray.add(markerG);
 
         return true;
     }
@@ -88,6 +142,66 @@ public class MyRenderer extends ARRenderer {
                 cube2.draw(gl);
                 gl.glPopMatrix();
             }
+
+            if (entry.getKey() == markerA) {
+                Log.e(TAG, "markerA - Drawing");
+                gl.glLoadMatrixf(entry.getValue(), 0);
+
+                gl.glPushMatrix();
+                gl.glRotatef(angle, 0.0f, 0.0f, 1.0f);
+                cube3.draw(gl);
+                gl.glPopMatrix();
+            }
+
+            if (entry.getKey() == markerB) {
+                Log.e(TAG, "markerB - Drawing");
+                gl.glLoadMatrixf(entry.getValue(), 0);
+
+                gl.glPushMatrix();
+                gl.glRotatef(angle, 0.0f, 0.0f, 1.0f);
+                cube4.draw(gl);
+                gl.glPopMatrix();
+            }
+
+            if (entry.getKey() == markerC) {
+                Log.e(TAG, "markerC - Drawing");
+                gl.glLoadMatrixf(entry.getValue(), 0);
+
+                gl.glPushMatrix();
+                gl.glRotatef(angle, 0.0f, 0.0f, 1.0f);
+                cube5.draw(gl);
+                gl.glPopMatrix();
+            }
+
+            if (entry.getKey() == markerD) {
+                Log.e(TAG, "markerD - Drawing");
+                gl.glLoadMatrixf(entry.getValue(), 0);
+
+                gl.glPushMatrix();
+                gl.glRotatef(angle, 0.0f, 0.0f, 1.0f);
+                cube6.draw(gl);
+                gl.glPopMatrix();
+            }
+
+            if (entry.getKey() == markerF) {
+                Log.e(TAG, "markerF - Drawing");
+                gl.glLoadMatrixf(entry.getValue(), 0);
+
+                gl.glPushMatrix();
+                gl.glRotatef(angle, 0.0f, 0.0f, 1.0f);
+                cube7.draw(gl);
+                gl.glPopMatrix();
+            }
+
+            if (entry.getKey() == markerG) {
+                Log.e(TAG, "markerG - Drawing");
+                gl.glLoadMatrixf(entry.getValue(), 0);
+
+                gl.glPushMatrix();
+                gl.glRotatef(angle, 0.0f, 0.0f, 1.0f);
+                cube8.draw(gl);
+                gl.glPopMatrix();
+            }
         }
     }
 
@@ -109,30 +223,3 @@ public class MyRenderer extends ARRenderer {
         return transformationArray;
     }
 }
-        /*if (ARToolKit.getInstance().queryMarkerVisible(markerHiro)) {
-            Log.e(TAG, "MarkerHiro - Drawing");
-            gl.glLoadMatrixf(ARToolKit.getInstance().queryMarkerTransformation(markerHiro), 0);
-
-            gl.glPushMatrix();
-            gl.glRotatef(angle, 0.0f, 0.0f, 1.0f);
-            cube1.draw(gl);
-            gl.glPopMatrix();
-
-            if (spinning) {
-                angle += 5.0f;
-            }
-        }
-
-        if (ARToolKit.getInstance().queryMarkerVisible(markerKanji)) {
-            Log.e(TAG, "markerKanji - Drawing");
-            gl.glLoadMatrixf(ARToolKit.getInstance().queryMarkerTransformation(markerKanji), 0);
-
-            gl.glPushMatrix();
-            gl.glRotatef(angle, 0.0f, 0.0f, 1.0f);
-            cube2.draw(gl);
-            gl.glPopMatrix();
-
-            if (spinning) {
-                angle += 5.0f;
-            }
-        }*/
